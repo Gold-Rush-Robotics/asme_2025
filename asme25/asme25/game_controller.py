3391dbc49efee978cca70543c482ad74b12531fa
Sean Devlin <sdevlin1@charlotte.edu>
2025-04-03 19:07:51 -0400

    updated game file with reccomened changes and it runs yippee :tada:

--- asme25/asme25/game_controller.py
+++ asme25/asme25/game_controller.py
@@ -4,23 +4,17 @@ from rclpy.node import Node
 
 from std_msgs.msg import String, Int64
 from sensor_msgs.msg import JointState
+from asme25_msgs.msg import Servo , Marbles
 
 from enum import Enum
 
-class Marble(Enum):
-    BRASS = 1
-    NYLON = 2
-    STEEL = 3
 
-class Servo(Enum):
-    LEFT = 1
-    RIGHT = 2
 
 SERVO_PATHS = {
-    # paths are temp right now
-    Marble.BRASS: [Servo.LEFT, Servo.LEFT, Servo.LEFT],
-    Marble.NYLON: [Servo.LEFT, Servo.LEFT, Servo.LEFT],
-    Marble.STEEL: [Servo.LEFT, Servo.LEFT, Servo.LEFT]
+    #paths are temp right now
+    Marbles.BRASS: [Servo.LEFT, Servo.LEFT, Servo.LEFT],
+    Marbles.NYLON: [Servo.LEFT, Servo.LEFT, Servo.LEFT],
+    Marbles.STEEL: [Servo.LEFT, Servo.LEFT, Servo.LEFT]
 }
 
 
@@ -31,28 +25,33 @@ class GameController(Node):
     def __init__(self):
         super().__init__('game_controller')
         self.servo_commands_publisher = self.create_publisher(Servo, 'servo_commands', 10)
-        self.joint_state_subscriber = self.create_subscription(JointState, 'robot_joints/states', 10)
-        self.marble_subscriber = self.create_subscription(Marbles, 'marbles', 10)
+        self.joint_state_subscriber = self.create_subscription(JointState, 'robot_joints/states', self.joint_state_callback, 10)
+        self.marble_subscriber = self.create_subscription(Marbles, 'marbles', self.marble_callback,  10)
         self.hmi_start_stop = self.create_subscription(String, 'hmi_start_stop', self.on_start, 10)
         self.solenoid_command_publsiher = self.create_publisher(Int64, 'solenoid_commands', 10)
     
     def marble_callback(self, msg):
-        #when we get marbles we need to check for new marbles
+        """"
+        when we get marbles we need to check for new marbles
+        """
         marble = msg[0]
         path = SERVO_PATHS[marble]
-        self.servo_commands_publisher.publish(path)
+        self.servo_commands_publisher.publish(path.value)
         # TODO: Change 1 to the ID of the solenoid to move
         self.solenoid_command_publsiher.publish(1)
+        
 
     def joint_state_callback(self, msg):
+    
+    
         pass
-
-
-    # Processes st messages sent by the HMI
+    
 
     def on_start(self, msg: String) -> None:
+        """
+        Processes start messages sent by the HMI    
+        """
         pass
-
 
 def main(args=None):
     rclpy.init(args=args)
